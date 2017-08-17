6c61e859c7cee249fbcaea801b8e25aafa07e66b
SansFr <sans.frontieres@protonmail.com>
2017-08-17 22:47:08 +0300

    Compliant symbol operations

--- src/Eval.hs
+++ src/Eval.hs
@@ -4,6 +4,8 @@ import Value
 import Parser
 
 eval :: Value -> Value
+
+-- things that eval to themselves: all atoms and quoted things
 eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
@@ -11,22 +13,39 @@ eval val@(Atom _) = val
 eval val@(Character _) = val
 eval (List [Atom "quote", val]) = val
 
+-- unquoted lists are function applications
+--  and eval to the result of the application
 eval (List (Atom func : args)) = apply func $ map eval args
 apply :: String -> [Value] -> Value
 apply func args = maybe (Bool False) ($ args) $ lookup func primitives
 
-primitives :: [(String, [Value] -> Value)]
-primitives = [("+", numericBinop (+)),
+car :: Value -> Value
+car (List (x:xs)) = x
+cdr :: Value -> Value
+cdr (List (_:xs)) = List xs
+listPrimitives = [("car", unaryOp car),
+                  ("cdr", unaryOp cdr)]
+
+numberPrimitives = [("+", numericBinop (+)),
               ("-", numericBinop (-)),
               ("*", numericBinop (*)),
               ("/", numericBinop div),
               ("mod", numericBinop mod),
               ("quotient", numericBinop quot),
-              ("remainder", numericBinop rem),
-              ("symbol?", unaryOp ((===) (Atom "")))]
+              ("remainder", numericBinop rem)]
+
+symbolPredicate = (===) (Atom "")
+symbol2string (Atom str) = String str
+symbolPrimitives :: [(String, [Value] -> Value)]
+symbolPrimitives = [("symbol?", unaryOp symbolPredicate),
+                    ("symbol->string", unaryOp symbol2string)]
+
+primitives = numberPrimitives ++
+             symbolPrimitives ++
+             listPrimitives
 
 unaryOp :: (Value -> Value) -> [Value] -> Value
-unaryOp op (x:[]) = op x
+unaryOp op [x] = op x
 --unaryOp op xs = TODO error or something
 
 numericBinop :: (Integer -> Integer -> Integer) -> [Value] -> Value
