f6b73ad47230e669b3bd09f7973278d92a5c3979
SansFr <sans.frontieres@protonmail.com>
2017-09-04 01:20:35 +0300

    Add "if" and logical operations to tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -32,14 +32,14 @@ module Main where
       ("Number", "23", Number 23),
       ("String", "\"abc\"", String "abc"),
       ("Atom", "atm", Atom "atm"),
-      ("Function Application", "(func 1 2 3)", List [Atom "func", Number 1, Number 2, Number 3]),
-      ("List", "(1 2 3)", List [Number 1, Number 2, Number 3]),
-      ("Quoted List", "'(1 2 3)", List [Atom "quote", List [Number 1, Number 2, Number 3]]),
       ("Character", "#\\c", Character 'c'),
       ("Character.Space", "#\\space", Character ' '),
       ("Character.Newline", "#\\newline", Character '\n'),
       ("Boolean.True", "#t", Bool True),
-      ("Boolean.False", "#f", Bool False)
+      ("Boolean.False", "#f", Bool False),
+      ("List", "(1 2 3)", List [Number 1, Number 2, Number 3]),
+      ("Quoted List", "'(1 2 3)", List [Atom "quote", List [Number 1, Number 2, Number 3]]),
+      ("Function Application", "(func 1 2 3)", List [Atom "func", Number 1, Number 2, Number 3])
     ]
 
   primitiveSpecs :: [EvalSpecDef]
@@ -50,12 +50,24 @@ module Main where
       ("Divide",   List [Atom "/", Number 23, Number 3],         Number 7),
       ("Modulo",   List [Atom "mod", Number 22, Number 3],       Number 1),
       ("Quotient", List [Atom "quotient", Number 22, Number 3],  Number 7),
-      ("Remainder",List [Atom "remainder", Number 22, Number 3], Number 1)
+      ("Remainder",List [Atom "remainder", Number 22, Number 3], Number 1),
+
+      ("<",  List [Atom "<",  Number 1, Number 2], Bool True),
+      (">",  List [Atom ">",  Number 1, Number 2], Bool False),
+      ("<=", List [Atom "<=", Number 1, Number 1], Bool True),
+      (">=", List [Atom ">=", Number 1, Number 1], Bool True),
+      ("&&", List [Atom "&&", Bool True, Bool False], Bool False),
+      ("||", List [Atom "||", Bool True, Bool False], Bool True),
+
+      ("If", List [Atom "if",
+        List [Atom ">", Number 3, Number 2],
+        List [Atom "+", Number 1, Number 2],
+        List [Atom "*", Number 3, Number 5]], Number 3)
     ]
 
   predicateSpecs :: [EvalSpecDef]
   predicateSpecs = [
-      ("Symbol predicate", List [Atom "symbol?", Atom "a"], Bool False),
+      ("Symbol predicate", List [Atom "symbol?", Atom "a"], Bool True),
       ("List predicate",   List [Atom "list?", List [Atom "quote", List []]], Bool True),
       ("Number predicate", List [Atom "number?", Number 1], Bool True),
       ("String predicate", List [Atom "string?", String "a"], Bool True),
