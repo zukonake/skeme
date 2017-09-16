84a3a21e2ec72ebd9caf1cc19f4ba07ab1d66916
zukonake <zukonake@protonmail.com>
2017-09-16 20:40:34 +0200

    Added if primitive

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -5,19 +5,19 @@ import Value
 import LangError
 import Operator
 
-type Primitive = (String, [Value] -> ThrowsError Value)
+type Primitive = [Value] -> ThrowsError Value
 
-cons :: [Value] -> ThrowsError Value
+cons :: Primitive
 cons [x, List xs] = return $ List $ x:xs
 cons [_, notList] = notOfType "List" notList
 cons args = throwError $ NumArgs 2 args
 
-listPrimitives :: [Primitive]
+listPrimitives :: [(String, Primitive)]
 listPrimitives = [("car", unaryOp unpackList id head),
                   ("cdr", unaryOp unpackList List tail),
                   ("cons", cons)]
 
-booleanPrimitives :: [Primitive]
+booleanPrimitives :: [(String, Primitive)]
 booleanPrimitives = [("<", binOp unpackNum Bool (<)),
                      (">", binOp unpackNum Bool (>)),
                      ("<=", binOp unpackNum Bool (<=)),
@@ -25,7 +25,7 @@ booleanPrimitives = [("<", binOp unpackNum Bool (<)),
                      ("&&", binOp unpackBool Bool (&&)),
                      ("||", binOp unpackBool Bool (||))]
 
-numberPrimitives :: [Primitive]
+numberPrimitives :: [(String, Primitive)]
 numberPrimitives = [("+", binOp unpackNum Number (+)),
                     ("-", binOp unpackNum Number (-)),
                     ("*", binOp unpackNum Number (*)),
@@ -34,7 +34,7 @@ numberPrimitives = [("+", binOp unpackNum Number (+)),
                     ("quotient", binOp unpackNum Number quot),
                     ("remainder", binOp unpackNum Number rem)]
 
-typePrimitives :: [Primitive]
+typePrimitives :: [(String, Primitive)]
 typePrimitives = [("symbol?", unaryOp unpackValue id $ (===) Atom {}),
                   ("list?", unaryOp unpackValue id $ (===) List {}),
                   ("number?", unaryOp unpackValue id $ (===) Number {}),
@@ -43,8 +43,18 @@ typePrimitives = [("symbol?", unaryOp unpackValue id $ (===) Atom {}),
                   ("bool?", unaryOp unpackValue id $ (===) Bool {}),
                   ("symbol->string", unaryOp unpackAtom String id)]
 
-primitives :: [Primitive]
+lispIf :: Primitive
+lispIf [Bool True, true, false] = return $ true
+lispIf [Bool False, true, false] = return $ false
+lispIf (nonBool:_) = notOfType "Bool" nonBool
+lispIf args = throwError $ NumArgs 3 args
+
+conditionalPrimitives :: [(String, Primitive)]
+conditionalPrimitives = [("if", lispIf)]
+
+primitives :: [(String, Primitive)]
 primitives = numberPrimitives ++
              typePrimitives ++
              listPrimitives ++
-             booleanPrimitives
+             booleanPrimitives ++
+             conditionalPrimitives
